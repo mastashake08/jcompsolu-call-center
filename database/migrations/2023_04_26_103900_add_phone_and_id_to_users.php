49204e9f491b3f70f5b2005a318b4948f1679203
Hacker God <jyrone.parker@gmail.com>
2023-04-26 06:40:52 -0400

    change db

---
+++ database/migrations/2023_04_26_103900_add_phone_and_id_to_users.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->string('email')->nullable()->change();
+            $table->string('stripe_account_id')->nullable();
+            $table->string('phone_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
