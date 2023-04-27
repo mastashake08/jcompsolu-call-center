f0f4c2731cd5d255fdb2487330624e971819c8ce
Hacker God <jyrone.parker@gmail.com>
2023-04-26 23:01:32 -0400

    reauth

--- app/Http/Controllers/StripeConnectController.php
+++ app/Http/Controllers/StripeConnectController.php
@@ -35,7 +35,19 @@ class StripeConnectController extends Controller
 
     public function finishOnboarding (Request $request) {
       echo 'You may close this window!';
-      
+
+      // TODO: grab user account and transfer funds
+    }
+
+    public function reauth (Request $request) {
+      $account_id = $request->account_id;
+      $links = $this->stripe->accountLinks->create([
+          'account' => $account_id,
+          'refresh_url' => secure_url('/stripe/reauth?account_id='.$account->id),
+          'return_url' => secure_url('/stripe/return?account_id='.$account->id),
+          'type' => 'account_onboarding',
+        ]);
+      return redirect($links->url);
       // TODO: grab user account and transfer funds
     }
 }
